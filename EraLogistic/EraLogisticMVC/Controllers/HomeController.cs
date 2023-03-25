bcd05d409c824dcd63751e8ecdf359055dbed520
Fehm1 <fehminhuseynov03@gmail.com>
2023-03-25 15:12:34 +0400

    Init

---
+++ EraLogistic/EraLogisticMVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using EraLogisticMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EraLogisticMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
